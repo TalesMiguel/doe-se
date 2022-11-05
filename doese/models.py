ca87d4b1341eaface5bece12bd447393c80c4417
Tales Miguel <talesmz@hotmail.com>
2022-11-05 19:52:40 -0300

    model Instituicoes

--- doese/models.py
+++ doese/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Instituicoes(models.Model):
+    nome = models.CharField(max_length=256)
+    CNPJ = models.CharField(max_length=32)
+    telefone = models.CharField(max_length=32)
+    email = models.EmailField(max_length=256)
+    endereco = models.CharField(max_length=512)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
