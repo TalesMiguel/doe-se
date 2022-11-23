706b1d636954ddb0dfa0dc1767a52ae534e44286
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2022-11-23 16:11:06 -0300

    Update models.py

--- doese/models.py
+++ doese/models.py
@@ -3,6 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class User(models.model):
+    nome = models.CharField(max_length=50)
+    CEP = models.IntegerField
+    data_de_nascimento = models.DateField
+    def __str__(self):
+        return self.nome
+
+class Historico(models.model):
+    tipo = models.IntegerField
+    local = models.CharField(max_length=128)
+    CEP_local = models.IntegerField
+    data_inicio = models.DateField
+    data_termino = models.DateField
+    valor = models.IntegerField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Instituicoes(models.Model):
     nome = models.CharField(max_length=256)
     CNPJ = models.CharField(max_length=32)
@@ -10,3 +26,14 @@ class Instituicoes(models.Model):
     email = models.EmailField(max_length=256)
     endereco = models.CharField(max_length=512)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nome
+
+class Acao(models.model):
+    intistuicao = models.ForeignKey(Instituicoes, on_delete=models.CASCADE)
+    tipo = models.IntegerField
+    local = models.CharField(max_length=128)
+    CEP_local = models.IntegerField
+    data_inicio = models.DateTimeField
+    data_termino = models.DateTimeField
+    concluido = models.BooleanField
