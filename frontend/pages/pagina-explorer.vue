efe4333d462887b92f723f67648541ebfa9c86ad
Teka <gabrielteka9@gmail.com>
2022-12-06 18:19:47 -0300

    pagina explorar e instituicao

---
+++ frontend/pages/pagina-explorer.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-main>
+    <v-container fluid>
+      <v-app-bar color="pink">
+        <v-toolbar dense width="10">
+          <v-text-field hide-details single-line clearable>
+          </v-text-field>
+              <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+              </v-btn>
+        </v-toolbar>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+      <v-list>
+        <v-list-item-group>
+            <template v-for="(item, index) in items">
+              <v-list-item style="width: 100vw;" v-if="!active" @click="inst_home">
+                <v-list-item-avatar>
+                  <v-icon size="50" color="#c6535f" v-if="item.temImagem">mdi-account-circle
+                  </v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.Instituicao" align="left">
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="text--primary" v-text="item.tipo" align="left">
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list-item-group>
+        </v-list>
+    </v-container>
+  </v-main>
+</template>
+
+
+<script>
+export default {
+  name: 'pagina-explorer',
+  layout: 'navbar',
+  data: () => ({
+    selected: [2],
+    items: [
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+        {
+          temImagem: true,
+          tipo: 'Tipo de instituição',
+          Instituicao: 'Instituição zzzz-zzzz',
+        },
+    ],
+  }),
+  methods: {
+    inst_home (){
+      this.$router.push('./inst-home')
+    }
+  }
+}
+
+</script>
+<style>
+.center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ frontend/pages/pagina-instituicao.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container fluid class="my-16">
+        <v-card flat>
+            <v-img class="text aling-end" src="https://raw.githubusercontent.com/Riiber0/doe-se/main/frontend/assets/imagem-3.jpg" max-height="20vh">
+            </v-img>
+        </v-card>
+        <v-container fluid class="my-20">
+            <v-row>
+                <v-col>
+                    <v-card flat width="24vw" height="400" color="#d3d3d3" tile align="center">
+                        <v-avatar class="profile" size="164">
+                            <v-icon size="164" color="#c6535f" v-if="temImagem">mdi-account-circle
+                                </v-icon>
+                        </v-avatar>
+                        <h1 style="color: #d6535f">{{nomeInst}}</h1>
+                        <v-container>
+                            <v-row>
+                                <v-col>
+                                    <div align="right">
+                                        <v-icon color="green" size="60" >mdi-food-apple</v-icon>
+                                    </div>
+                                </v-col>
+                                <v-col class="my-3">
+                                    <div align="left">
+                                        <h2>Comida</h2>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-container fluid class="my-20">
+                    <v-card flat width="60vw" color="#d3d3d3" tile align="center">
+                        <v-row>
+                            <v-col
+                                v-for="n in 9"
+                                :key="n"
+                                class="my-1"
+                                cols="4">
+                                <v-img
+                                    :src="`https://picsum.photos/500/300?image=${n * 5 + 5}`"
+                                    aspect-ratio="1.5"
+                                    class="grey lighten-2">
+                                </v-img>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-container>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'pagina-instituicao',
+  layout: 'navbar',
+  data: function () {
+    return {
+      temImagem: true,
+      nomeInst: 'placeHolder'
+    }
+  }
+}
+</script>
