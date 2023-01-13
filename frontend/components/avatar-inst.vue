71c61d1e1ac87d6645a9b8e9c6a0630456361335
joaop01 <joaop80105@gmail.com>
2023-01-12 23:17:59 -0300

    hey

--- frontend/components/avatar-inst.vue
+++ frontend/components/avatar-inst.vue
@@ -1,11 +1,31 @@
 <template>
-  <v-card flat width="24vw" color="#d3d3d3" tile align="center">
-    <v-avatar class="profile" size="164">
+<v-main>
+  <v-row>
+    <v-col md=3>
+  <v-card flat width="20vw" height="35vh" color="#d3d3d3" tile align="center">
+    <v-avatar class="profile mt-10" size="164">
       <v-icon size="164" color="#c6535f" v-if="temImagem">mdi-account-circle
         </v-icon>
     </v-avatar>
-    <h2 style="color: #d6535f">{{nomeInst}}</h2>
   </v-card>
+    </v-col>
+    <v-col class="d-flex" md=9>
+      <v-img align-left :src="image3" max-height="35vh" max-width="68vw">
+      </v-img>
+    </v-col>
+  </v-row>
+  <v-row class="mt-12" fluid>
+    <v-col md=12>
+    <v-card flat width="92vw" height="80vh" color="#e9e9e9" tile align="center">
+      <v-card-text>
+        <v-col>
+        <h1> Publicações </h1>
+        </v-col>
+      </v-card-text>
+    </v-card>
+    </v-col>
+  </v-row>
+</v-main>
 </template>
 <script>
 export default {
@@ -13,7 +33,7 @@ export default {
   data: function () {
     return {
       temImagem: true,
-      nomeInst: 'placeHolder'
+      image3: require('../assets/imagem-3.jpg'),
     }
   }
 }
