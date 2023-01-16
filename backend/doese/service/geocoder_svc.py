cc9add63dce3093c0453ee1e2e803e3356763c37
Felipe Rosa <feliperibeirorosa@hotmail.com>
2023-01-15 22:53:57 -0300

    chave

--- backend/doese/service/geocoder_svc.py
+++ backend/doese/service/geocoder_svc.py
@@ -1,7 +1,7 @@
 import geocoder
 
 def converter(endereco):
-    g = geocoder.google(request.POST['endereco'], key='AIzaSyDqoLIqGcBNiEcPpxSRcKTliFu-HJk-LoE', method='places')
+    g = geocoder.google(endereco, key='AIzaSyDqoLIqGcBNiEcPpxSRcKTliFu-HJk-LoE', method='places')
     #g = geocoder.osm(endereco)
     resultado = [g.latlng[0], g.latlng[1]]
     return resultado
