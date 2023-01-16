65adec1de0c91f7eea3a658ffbc8a64b7a26fef1
Felipe Rosa <feliperibeirorosa@hotmail.com>
2023-01-15 23:09:55 -0300

    mapa google

--- frontend/plugins/provider.js
+++ frontend/plugins/provider.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import "leaflet/dist/leaflet.css";
+import { GoogleProvider } from 'leaflet-geosearch';
+
+const provider = new GoogleProvider({ apiKey: 'AIzaSyDqoLIqGcBNiEcPpxSRcKTliFu-HJk-LoE' });
 
 export default ({ app }, inject) => {
-    inject('osm', new OpenStreetMapProvider())
+    inject('osm', provider)
   }
