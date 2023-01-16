0d5fe995c389f0bd06f2351d30e4ef23d88e6cd5
Felipe Rosa <feliperibeirorosa@hotmail.com>
2023-01-15 23:26:09 -0300

    cadastro user

--- frontend/pages/cadastro-usuario.vue
+++ frontend/pages/cadastro-usuario.vue
@@ -4,77 +4,90 @@
             elevation="10"
             color="#F6E2E2"
             outlined
-            width="600"
-            height="650"
-        ><h1 style="color:#000">Doe-se é melhor com você!</h1>
+            width="30vw"
+            height="80vh"
+        >
         <div class="center" align="center">
-          <v-card
-            elevation="0"
-            color="#c6535f"
+        <h2>Cadastro</h2>
+          <v-form
+            ref="formUser"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+            v-model="name"
+            :counter="25"
+            :rules="nameRules"
+            label="Nome"
+            required
             outlined
-            width="400"
-          ><h2>Cadastro</h2>
-            <v-form
-              ref="formUser"
-              v-model="valid"
-              lazy-validation
+            filled
+            background-color="#f6f6f6"
+            style="width:25vw"
             >
-              <v-text-field
-              v-model="name"
-              :counter="25"
-              :rules="nameRules"
-              label="Nome"
+            </v-text-field>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
               required
-              >
-              </v-text-field>
-              <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="cpf"
-                :rules="cpfRules"
-                label="CPF"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="celular"
-                :rules="celularRules"
-                label="Telefone celular"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="senha"
-                :rules="senhaRules"
-                label="Digite sua senha"
-                type="password"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="confirma"
-                :rules="confirmaRules"
-                label="Confirme sua senha"
-                type="password"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="dataNasc"
-                :rules="dataNascRules"
-                label="Data de nascimento"
-                type="date"
-              ></v-text-field>
-              <div align="right"><v-btn
-                :disabled="!valid"
-                color="#d8d5d5"
-                class="mr-4"
-                @click="validate"
-              >
-                Pronto
-              </v-btn></div>
+              outlined
+              filled
+              background-color="#f6f6f6"
+            ></v-text-field>
+            <v-text-field
+              v-model="cpf"
+              :rules="cpfRules"
+              label="CPF"
+              required
+              outlined
+              filled
+              background-color="#f6f6f6"
+            ></v-text-field>
+            <v-text-field
+              v-model="celular"
+              :rules="celularRules"
+              label="Telefone celular"
+              required
+              outlined
+              filled
+              background-color="#f6f6f6"
+            ></v-text-field>
+            <v-text-field
+              v-model="senha"
+              :rules="senhaRules"
+              label="Digite sua senha"
+              type="password"
+              required
+              outlined
+              filled
+              background-color="#f6f6f6"
+            ></v-text-field>
+            <v-text-field
+              v-model="confirma"
+              :rules="confirmaRules"
+              label="Confirme sua senha"
+              type="password"
+              required
+              outlined
+              filled
+              background-color="#f6f6f6"
+            ></v-text-field>
+            <v-text-field
+              v-model="dataNasc"
+              :rules="dataNascRules"
+              label="Data de nascimento"
+              type="date"
+            ></v-text-field>
+            <div align="right"><v-btn
+              :disabled="!valid"
+              color="#d8d5d5"
+              class="mr-4"
+              @click="validate"
+            >
+              Pronto
+            </v-btn></div>
             </v-form>
-          </v-card>
         </div>
 
         </v-card>
