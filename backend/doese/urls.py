5477f898a111ae5faaa21eba6123a95b08f65800
Tales Miguel <talesmz@hotmail.com>
2023-01-15 22:15:56 -0300

    endpoints de instituicao na url, novos endpoints de acao

--- backend/doese/urls.py
+++ backend/doese/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path('add-acao/', views.add_acao, name='add acao'),
     path('get-acao/', views.get_acao, name='get acao'),
+    path('edit-acao/', views.update_acao, name='edit acao'),
+    path('delete-acao/', views.delete_acao, name='delete acao'),
+    path('add-instituicao/', views.create_instituicao, name='create instituicao'),
+    path('list-instituicao/', views.list_instituicao, name='list instituicao'),
+    path('edit-instituicao/', views.update_instituicao, name='update instituicao'),
+    path('delete-instituicao/', views.delete_instituicao, name='delete instituicao'),
     path('get-geojson/', views.get_geojson, name='get geojson'),
     path('get-coord/', views.get_coord, name='get coord'),
 ]
